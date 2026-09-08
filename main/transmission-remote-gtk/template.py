c573fe8f7f16dc52b6b7b307c3ec9382c9064c6d
Jose Arellano <jose.4rellano@gmail.com>
2026-09-08 16:53:32 -0300

    main/transmission-remote-gtk: update to 1.7.1

--- main/transmission-remote-gtk/template.py
+++ main/transmission-remote-gtk/template.py
@@ -1,19 +1,22 @@
 pkgname = "transmission-remote-gtk"
-pkgver = "1.6.0"
+pkgver = "1.7.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
-    "appstream-glib",
+    "appstream",
     "desktop-file-utils",
     "gettext",
     "meson",
     "pkgconf",
+    "python-docutils",
 ]
 makedepends = ["gtk+3-devel", "json-glib-devel", "libsoup-devel"]
 pkgdesc = "GTK client for remote management of Transmission torrent client"
 license = "GPL-2.0-or-later"
 url = "https://github.com/transmission-remote-gtk/transmission-remote-gtk"
-source = (
-    f"{url}/releases/download/{pkgver}/transmission-remote-gtk-{pkgver}.tar.xz"
-)
-sha256 = "b090844f6a482e6f3588070ff3fdd54b79e8f85df02b39853cfb01fccee10cac"
+source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "5ef98bd96b3b77eb3880474a2d904e316bb29cbd22dfa0ca85d43b04a28fba34"
+
+if self.profile().cross:
+    # gtk+3-devel only pulls glib-devel into the target sysroot
+    hostmakedepends += ["glib-devel"]
