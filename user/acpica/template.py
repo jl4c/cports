ee1621d999a902d37ed775d50f51869279d565de
Jose Arellano <jose.4rellano@gmail.com>
2026-09-09 09:32:55 -0300

    user/acpica: switch source to install license, silence clang warnings, fix license, drop update.py

--- user/acpica/template.py
+++ user/acpica/template.py
@@ -1,15 +1,21 @@
 pkgname = "acpica"
 pkgver = "20260408"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
-# disable builtin flags that are supplied by cbuild (opt, fortify)
-make_build_args = ["OPT_CFLAGS="]
+make_build_args = ["NOFORTIFY=TRUE", "NOWERROR=TRUE"]
 make_use_env = True
 hostmakedepends = ["bison", "flex"]
 pkgdesc = "Intel ACPI Component Architecture utilities"
-license = "GPL-2.0-only OR BSD-3-Clause OR Intel-ACPI"
+license = "GPL-2.0-only OR BSD-3-Clause-acpica OR Intel-ACPI"
 url = "https://www.acpica.org"
-source = f"https://github.com/acpica/acpica/releases/download/{pkgver}/acpica-unix-{pkgver}.tar.gz"
-sha256 = "e66ceb26d6d514ce164fe22f5a4f7ca165cc38349d7a97f41a21f19b364647a2"
-# no tests; no license file
-options = ["!check", "!distlicense"]
+source = (
+    f"https://github.com/open-acpica/acpica/archive/refs/tags/{pkgver}.tar.gz"
+)
+sha256 = "ddc5d3e0f54030e2348484fff681861a161efb4e388e20631209574e7884ad39"
+tool_flags = {"CFLAGS": ["-Wno-unknown-warning-option"]}
+# no check target
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
